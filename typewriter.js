c02495c47b0c0f76e77005215fd8c9c562d20293
fddiashok <fddiashok@gmail.com>
2021-11-30 14:16:52 -0500

    initial Commit to Typewriter Aninmation

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+const { time } = require("console");
+
+const sentence = "hello there from lighthouse labs";
+let timer = 0
+
+for (const char of sentence) {
+  setTimeout(() =>
+    process.stdout.write(char)
+    , timer);
+  timer += 100
+}
+setTimeout(()=> process.stdout.write('\n'),timer);
